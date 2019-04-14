4ca6a7e5885bc17683d7f10812ad74dea3e5dfb5
T3MUJIN <rifopangemanan@yahoo.com>
2019-04-14 10:56:37 -0800

    first commit done drawer toolber

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,45 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <v-navigation-drawer absolute temporary="" v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>streetview</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Lia Tampa</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon
+        @click.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up"
+      ></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        Baku-Dapa
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-icon left>streetview</v-icon>
+        <v-btn flat>Lia Tampa</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
 
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
+<style></style>
 
 <script>
-
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
       //
-    }
+      sideNav: false
+    };
   }
-}
+};
 </script>
